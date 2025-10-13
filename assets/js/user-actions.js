3ac9841a5edd9ce511e708020ad106a39d6cf51a
RogueNinja <111859679+Sparks1893@users.noreply.github.com>
2025-10-13 15:16:18 +0100

    Update user-actions.js

--- assets/js/user-actions.js
+++ assets/js/user-actions.js
@@ -14,3 +14,28 @@ jQuery(document).ready(function($){
         });
     });
 });
+// Favorite toggle
+$('.plm-btn-favorite').click(function(){
+    let book = $(this).data('book');
+    let button = $(this);
+    $.post(plm_ajax.ajax_url, {
+        action:'plm_toggle_favorite',
+        book_id:book
+    }, function(response){
+        if(response === 'added') button.text('❤️ Favorited');
+        else button.text('❤️ Favorite');
+    });
+});
+
+// Like toggle
+$('.plm-btn-like').click(function(){
+    let book = $(this).data('book');
+    let button = $(this);
+    $.post(plm_ajax.ajax_url, {
+        action:'plm_toggle_like',
+        book_id:book
+    }, function(response){
+        if(response === 'added') button.text('👍 Liked');
+        else button.text('👍 Like');
+    });
+});
